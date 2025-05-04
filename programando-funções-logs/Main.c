06680f48db0fdb8c3ca78eca70dec42658158d23
Victor Augusto De Aquino Silvério <d2025016677@unifei.edu.br>
2025-05-04 08:37:24 +0000

    Funções feitas

--- programando-funções-logs/Main.c
+++ programando-funções-logs/Main.c
@@ -26,3 +26,29 @@ const char* current_time_str() {
     return buffer;
 }
 
+void init_log() {
+    gerar_nome_log(nomedolog, sizeof(nomedolog));
+    log_file = fopen(nomedolog, "a");
+    if (log_file == NULL) {
+        perror("Erro ao abrir o arquivo de log");
+        exit(EXIT_FAILURE);
+    }
+    fprintf(log_file, "Inicio\n", current_time_str());
+    fflush(log_file);
+}
+
+void escreverlog(const char *mensagem) {
+    if (log_file != NULL) {
+        fprintf(log_file, "[%s] %s\n", current_time_str(), mensagem);
+        fflush(log_file);
+    }
+}
+
+void close_log() {
+    if (log_file != NULL) {
+        escreverlog("Final-Log");
+        fclose(log_file);
+        log_file = NULL;
+    }
+}
+
