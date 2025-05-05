3fa9913da40544fb4c69e0c32d2b362e183fe2d9
Victor Augusto De Aquino Silvério <d2025016677@unifei.edu.br>
2025-05-05 12:07:24 +0000

    logs etapa 4

--- etapa4-integração-local/src/Main.c
+++ etapa4-integração-local/src/Main.c
@@ -14,6 +14,74 @@
 #include "aes_cmac.h"
 #include "Config.h"
 
+// Início bloco de log
+
+#include <stdarg.h>
+
+
+FILE *log_file = NULL;
+char nomedolog[64];
+
+void gerar_nome_log(char *buffer, size_t tamanho) {
+    time_t agora = time(NULL);
+    struct tm *tm_info = localtime(&agora);
+    strftime(buffer, tamanho, "logs/log_%Y%m%d_%H%M%S.txt", tm_info);
+}
+
+const char* current_time_str() {
+    static char buffer[32];
+    time_t now = time(NULL);
+    struct tm *t = localtime(&now);
+    strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", t);
+    return buffer;
+}
+
+void iniciarlog() {
+    gerar_nome_log(nomedolog, sizeof(nomedolog));
+    log_file = fopen(nomedolog, "a");
+    if (log_file == NULL) {
+        perror("Erro ao abrir o arquivo de log");
+        exit(EXIT_FAILURE);
+    }
+    fprintf(log_file, "========== NOVA EXECUÇÃO (%s) ==========\n\n", current_time_str());
+    fflush(log_file);
+}
+
+void escreverlog(const char *formato, ...) {
+    if (log_file != NULL) {
+        va_list args;
+        va_start(args, formato);
+        fprintf(log_file, "[%s] ", current_time_str());
+        vfprintf(log_file, formato, args);
+        fprintf(log_file, "\n");
+        va_end(args);
+        fflush(log_file);
+    }
+}
+
+void fecharlog() {
+    if (log_file != NULL) {
+        fprintf(log_file, "\n========== FINAL EXECUÇÃO (%s) ==========\n", current_time_str());
+        fclose(log_file);
+        log_file = NULL;
+    }
+}
+
+/*
+Comandos de execução:
+
+    iniciarlog();
+
+    escreverlog("texto aqui"); // as timestamp são adicionadas automaticamente na função
+
+    fecharlog();
+
+    escreverlog("texto aqui: %d operador", variavel);
+
+*/
+
+// Fim bloco de log
+
 int send_to_gateway(int sockfd, const uint8_t *packet, int packet_len, struct sockaddr_in *server_addr);
 int wait_for_ack(int sockfd);
 int save_config(const char *filename, const Config *configs, int device_count);
