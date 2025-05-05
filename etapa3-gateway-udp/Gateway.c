4623d8cb434bf65b442352bdedeba7e953e6b683
Victor Augusto De Aquino Silvério <d2025016677@unifei.edu.br>
2025-05-05 00:52:38 +0000

    Iniciando logs etapa 3

--- etapa3-gateway-udp/Gateway.c
+++ etapa3-gateway-udp/Gateway.c
@@ -9,6 +9,75 @@
 #define PORT 1700
 #define BUFFER_SIZE 1024
 
+// Início bloco de log
+
+#include <time.h>
+#include <stdarg.h>
+
+
+FILE *log_file = NULL;
+char nomedolog[64];
+
+void gerar_nome_log(char *buffer, size_t tamanho) {
+    time_t agora = time(NULL);
+    struct tm *tm_info = localtime(&agora);
+    strftime(buffer, tamanho, "logs/log_%Y%m%d_%H%M%S.txt", tm_info);
+}
+
+const char* current_time_str() {
+    static char buffer[32];
+    time_t now = time(NULL);
+    struct tm *t = localtime(&now);
+    strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", t);
+    return buffer;
+}
+
+void iniciarlog() {
+    gerar_nome_log(nomedolog, sizeof(nomedolog));
+    log_file = fopen(nomedolog, "a");
+    if (log_file == NULL) {
+        perror("Erro ao abrir o arquivo de log");
+        exit(EXIT_FAILURE);
+    }
+    fprintf(log_file, "========== NOVA EXECUÇÃO (%s) ==========\n\n", current_time_str());
+    fflush(log_file);
+}
+
+void escreverlog(const char *formato, ...) {
+    if (log_file != NULL) {
+        va_list args;
+        va_start(args, formato);
+        fprintf(log_file, "[%s] ", current_time_str());
+        vfprintf(log_file, formato, args);
+        fprintf(log_file, "\n");
+        va_end(args);
+        fflush(log_file);
+    }
+}
+
+void fecharlog() {
+    if (log_file != NULL) {
+        fprintf(log_file, "\n========== FINAL EXECUÇÃO (%s) ==========\n", current_time_str());
+        fclose(log_file);
+        log_file = NULL;
+    }
+}
+
+/*
+Comandos de execução:
+
+    iniciarlog();
+
+    escreverlog("texto aqui"); // as timestamp são adicionadas automaticamente na função
+
+    fecharlog();
+
+    escreverlog("texto aqui: %d operador", variavel);
+
+*/
+
+// Fim bloco de log
+
 int load_config(const char *filename, char *device_id, char *application_id) {
 	FILE *file = fopen(filename, "r");
 	if (!file) {
@@ -64,6 +133,9 @@ int load_config(const char *filename, char *device_id, char *application_id) {
 }
 
 int main() {
+
+	iniciarlog();
+
 	int sockfd;
 	struct sockaddr_in server_addr, client_addr;
 	socklen_t addr_len = sizeof(client_addr);
@@ -173,5 +245,8 @@ int main() {
 	}
 
 	close(sockfd);
+
+	fecharlog();
+	
 	return 0;
 }
