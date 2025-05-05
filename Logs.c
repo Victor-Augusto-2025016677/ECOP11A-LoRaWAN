f07a2e005f0b5b7ef37baf49f48d8580dc4d6013
Victor Augusto De Aquino Silvério <d2025016677@unifei.edu.br>
2025-05-05 00:03:54 +0000

    Iniciando novo codespace

--- Logs.c
+++ Logs.c
@@ -1,19 +1,17 @@
-#include <stdio.h> //geralmente já estão adicionadas
-#include <stdlib.h>
-#include <string.h>
-
-//Inicio bloco de log
+// Início bloco de log
 
 #include <time.h>
+#include <stdarg.h>
 
 FILE *log_file = NULL;
 char nomedolog[64];
 
-void gerar_nome_log(char *buffer, size_t tamanho) { //criação de nome com base no tempo
+void gerar_nome_log(char *buffer, size_t tamanho) { // criação de nome com base no tempo
     time_t agora = time(NULL);
     struct tm *tm_info = localtime(&agora);
     strftime(buffer, tamanho, "logs/log_%Y%m%d_%H%M%S.txt", tm_info);
 }
+
 const char* current_time_str() {
     static char buffer[32];
     time_t now = time(NULL);
@@ -21,6 +19,7 @@ const char* current_time_str() {
     strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", t);
     return buffer;
 }
+
 void iniciarlog() {
     gerar_nome_log(nomedolog, sizeof(nomedolog));
     log_file = fopen(nomedolog, "a");
@@ -31,12 +30,19 @@ void iniciarlog() {
     fprintf(log_file, "========== NOVA EXECUÇÃO (%s) ==========\n\n", current_time_str());
     fflush(log_file);
 }
-void escreverlog(const char *mensagem) {
+
+void escreverlog(const char *formato, ...) {
     if (log_file != NULL) {
-        fprintf(log_file, "[%s] %s\n", current_time_str(), mensagem);
+        va_list args;
+        va_start(args, formato);
+        fprintf(log_file, "[%s] ", current_time_str());
+        vfprintf(log_file, formato, args);
+        fprintf(log_file, "\n");
+        va_end(args);
         fflush(log_file);
     }
 }
+
 void fecharlog() {
     if (log_file != NULL) {
         fprintf(log_file, "\n========== FINAL EXECUÇÃO (%s) ==========\n", current_time_str());
@@ -45,15 +51,17 @@ void fecharlog() {
     }
 }
 
-//Fim bloco de log
-
 /*
 Comandos de execução:
 
     iniciarlog();
 
-    escreverlog("texto aqui"); //as timestamp são adicionadas automaticamente na função
+    escreverlog("texto aqui"); // as timestamp são adicionadas automaticamente na função
 
     fecharlog();
 
+    escreverlog("texto aqui: %d operador", variavel);
+
 */
+
+// Fim bloco de log
